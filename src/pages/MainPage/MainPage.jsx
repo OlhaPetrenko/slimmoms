c632089271a2a754ee42e9df66cf288df29cd228
Halyna Melnykova <galyamelnykova@gmail.com>
2022-09-22 00:02:18 +0300

    add modal in MainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,38 @@
+import { useState } from 'react';
+import Modal from 'components/Modal/Modal';
+import s from './MainPage.module.scss';
+
 const MainPage = () => {
-  return <div>Main</div>;
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      <button className={s.button} onClick={openModal}>
+        Start losing weight
+      </button>
+      {modalOpen && (
+        <Modal close={closeModal}>
+          <h2>Your recommended daily calorie intake is</h2>
+          <span>2800 ккал</span>
+          <h3 className={s.border}>Foods you should not eat</h3>
+          <ol>
+            <li>Flour products</li>
+            <li>Milk</li>
+            <li>Red meat</li>
+            <li>Smoked meats</li>
+          </ol>
+        </Modal>
+      )}
+    </>
+  );
 };
 
 export default MainPage;
