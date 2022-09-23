11f209149c7d1d7554ec7b6c5c95e46c49f9305b
LeonidGHA <stowk1992@gmail.com>
2022-09-23 09:47:20 +0300

    add fix Navigation

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -1,20 +1,20 @@
 import { NavLink } from 'react-router-dom';
-import s from "./UserInfo.module.scss"
+import s from './UserInfo.module.scss';
+const activeNav = ({ isActive }) => {
+  return isActive ? `${s.headerNavLink} ${s.active}` : s.headerNavLink;
+};
 
 const UserInfo = () => {
-
-  const activeNav = ({ isActive }) => {
-    return isActive ? `${s.headerNavLink} ${s.active}` : s.headerNavLink;
-  };
-
   return (
-
-    <div className={ s.headerNavContainer }>
-      <NavLink className={ activeNav } to="/diary">Diary</NavLink>
-      <NavLink className={ activeNav } to="/calculator">Calculator</NavLink>
+    <div className={s.headerNavContainer}>
+      <NavLink className={activeNav} to="/diary">
+        Diary
+      </NavLink>
+      <NavLink className={activeNav} to="/calculator">
+        Calculator
+      </NavLink>
     </div>
-
   );
-}
+};
 
 export default UserInfo;
