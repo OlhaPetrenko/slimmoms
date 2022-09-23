2e515385bfc9578aca418f6c2742c658fea4ecbe
Halyna Melnykova <galyamelnykova@gmail.com>
2022-09-23 23:23:17 +0300

    add modal in calculatorPage, add styles in common

--- src/components/DailyCalorieIntake/DailyCalorieIntake.jsx
+++ src/components/DailyCalorieIntake/DailyCalorieIntake.jsx
@@ -4,25 +4,38 @@ import s from './DailyCalorieIntake.module.scss';
 
 const DailyCalorieIntake = () => {
   const dailyRate = useSelector(state => state.dailyRate.dailyRate);
+  const summNotAllowed = useSelector(
+    state => state.dailyRate.notAllowedProducts
+  );
+  const LogInUser = useSelector(state => state.user.isLogin);
   const navigate = useNavigate();
 
+  const navigation = () => {
+    if (LogInUser) {
+      return navigate('/diary');
+    }
+    return navigate('/registration');
+  };
   return (
     <>
       <h2 className={s.title}>
         Your recommended daily
         <br /> calorie intake is
       </h2>
-      <p className={s.dailyRate}>
-        {Number.parseInt(dailyRate)} <span className={s.kcal}>kcal</span>
-      </p>
-      <h3 className={s.titleList}>Foods you should not eat</h3>
-      <ol>
-        <li className={s.item}>Flour products</li>
-        <li className={s.item}>Milk</li>
-        <li className={s.item}>Red meat</li>
-        <li className={s.item}>Smoked meats</li>
-      </ol>
-      <button className={s.button} onClick={() => navigate('/registration')}>
+      <div className={s.wrapper}>
+        <p className={s.dailyRate}>
+          {Number.parseInt(dailyRate)} <span className={s.kcal}>kcal</span>
+        </p>
+        <h3 className={s.titleList}>Foods you should not eat</h3>
+        <ol className={s.list}>
+          <li className={s.item}>{summNotAllowed[0]}</li>
+          <li className={s.item}>{summNotAllowed[1]}</li>
+          <li className={s.item}>{summNotAllowed[2]}</li>
+          <li className={s.item}>{summNotAllowed[3]}</li>
+        </ol>
+      </div>
+
+      <button className={s.button} onClick={navigation}>
         Start losing weight
       </button>
     </>
